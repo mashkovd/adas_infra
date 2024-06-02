1d67df6e934ed3743d1d3b44c3563fb560b15fca
Dmitrii.Mashkov <dmitrii.mashkov@libertexgroup.com>
2024-06-02 11:08:09 +0200

    change solution_stack_name

--- elasticbeanstalk/main.tf
+++ elasticbeanstalk/main.tf
@@ -62,7 +62,7 @@ resource "aws_elastic_beanstalk_application" "beanstalk_app" {
 resource "aws_elastic_beanstalk_environment" "beanstalk_env" {
   name                = var.environment_name
   application         = aws_elastic_beanstalk_application.beanstalk_app.name
-  solution_stack_name = "64bit Amazon Linux 2023 v4.0.12 running Python 3.11"
+  solution_stack_name = "64bit Amazon Linux 2023 v4.3.2 running Docker"
 
   setting {
     namespace = "aws:autoscaling:launchconfiguration"
